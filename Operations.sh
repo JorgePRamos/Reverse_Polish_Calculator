0ee7652dd7b3d264956f8d7535447d81a2b5e49d
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2018-11-22 21:04:14 +0100

    Clone of the development branch.
    Clone of the development branch in collaboration with my colleague Chemi3.

--- Operations.sh
+++ Operations.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# Comparacion del ultimo elemento == PRINT 
-# m[k]== PRINT
-# k-1 almacena el numero de elementos del array en este punto
+#========= UPDATED 09/11/2018====
+#====================================
+#====================================
+#FUNCIONES MATEMATICAS ===========
+#====================================
+#====================================
 
 function raiz(){
     if [[ "$#" -ne 1 ]];then
         echo "Syntax error"
-        #exit
+        exit 1
+    elif [[ "$1" -lt 0 ]]; then 
+        echo "Math error"
+        exit 2
     else 
         echo "sqrt ($1)"|bc
     fi
@@ -14,113 +20,137 @@ function raiz(){
 function mOdulo(){
     if [[ "$#" -ne 2 ]];then
         echo "Syntax error"
-        #exit
-    else 
+        exit 1
+    else  
         echo "$1 % $2"|bc
-        return
     fi
 
+}
+function potencia(){
+     if [[ "$#" -ne 2 ]];then
+        echo "Syntax error"
+        exit 1
+    else  
+        echo "$1 ^ $2"|bc
+    fi
 }
 function factorial (){
     if [[ "$#" -ne 1 ]];then
         echo "Syntax error"
         #exit
     else 
-        if [[ $1 -le 1 ]];then
-        echo 1
+        if [[ $1 -lt 0 ]];then
+            echo "Math error"
+            exit 2
+        elif [[ $1 -le 1 ]];then
+            echo 1
         else
-        
-        echo $(($1 * $(factorial $[$1-1])))
-    fi
+            echo $(($1 * $(factorial $[$1-1])))
+        fi
     fi 
       
 }
 function imprimir(){
     if [[ "$#" -ne 1 ]];then
         echo "Syntax error"
-        #exit
+        exit 1
     else 
         echo $1
     fi
 
 }
-if [[ ${m[((--k))]} != 'PRINT' ]];then 
-    echo 'Error print'
-    #exit 1
-#k almacena ahora el numero de elementos de array (0..k)
-#elif 
-# Proceder con la ejecucion
+function multiplicar(){
+    if [[ "$#" -ne 2 ]];then #atributos deben de ser ==2
+        echo "Syntax error"
+        exit 1
+    else 
+        echo $(($1 * $2))
+    fi
+}
+function  suma(){
+   if [[ "$#" -ne 2 ]];then #atributos deben de ser ==2
+        echo "Syntax error"
+        exit 1
+    else 
+        echo $(($1 + $2))
+    fi
+}
+function  resta(){
+   if [[ "$#" -ne 2 ]];then #atributos deben de ser ==2
+        echo "Syntax error"
+        exit 1
+    else 
+        echo $(($1 - $2))
+    fi
+}
 
-: <<'END'
-if ! [[ "$scale" =~ ^[0-9]+$ ]]
-    then
-        echo "Sorry integers only"
-fi
-
-IDEA RECURSIVIDAD
-funcion argumento
-    swict case
-        -
-        -   
-        -
-        -
-            funcion (argumento -k)
-caso base?? es el return de los numeros??
-return?? llamada a las funciones?
+function  division(){
+   if [[ "$#" -ne 2 ]];then #atributos deben de ser ==2
+        echo "Syntax error"
+        exit 1
+    else 
+        echo $(($1 / $2))
+    fi
+}
 
 
 
 
-END
 
+#====================================
+#====================================
+#RECURSIVIDAD_INUTIL ===========
+#====================================
+#====================================
+
+#if [[ ${m[((--k))]} != 'PRINT' ]];then 
 function operar(){
+
     if [[ "$#" -ne 1 ]];then
         echo "Syntax error"
         #exit
     else 
         if ! [[ "$1" =~ ^[0-9] ]];then
             if [[ "$1" = * ]];then
-                multiplicar `operar "$(pullPila)"`
+                echo $(multiplicar `operar "$(pullPila)"` `operar "$(pullPila)"`)
             elif [[ "$1" = ! ]];then
-                factorial `operar "$(pullPila)"` 
+                echo $(factorial `operar "$(pullPila)"`)
             case "$1" in
                 PRINT)
-                    imprimir `operar "$(pullPila)"` 
+                    echo $(imprimir $(operar $(pullPila))) #Experimento 2
                     ;;
                 +)
-                    suma `operar "$(pullPila)"` `operar "$(pullPila)"`
+                    echo $(suma `operar "$(pullPila)"` `operar "$(pullPila)"`) #Experimento
                     ;;
                 -)
-                    resta `operar "$(pullPila)"` `operar "$(pullPila)"`
+                    echo $( resta `operar "$(pullPila)"` `operar "$(pullPila)"`)
                     ;;
                 /)
-                    division `operar "$(pullPila)"` `operar "$(pullPila)"`
+                    echo $( division `operar "$(pullPila)"` `operar "$(pullPila)"`)
                     ;;
                 MOD)
-                    mOdulo `operar "$(pullPila)"` `operar "$(pullPila)"`
+                    echo $( mOdulo `operar "$(pullPila)"` `operar "$(pullPila)"`)
                     ;;
                 ^)
-                    potencia `operar "$(pullPila)"``operar "$(pullPila)"`
+                    echo $( potencia `operar "$(pullPila)"``operar "$(pullPila)"`)
                     ;;
                 SQRT)
-                    raiz `operar "$(pullPila)"`
+                    echo $( raiz `operar "$(pullPila)"`)
                     ;;
             
                 *)
-                    echo "Syntax error"
+                    echo "Syntax error: $1"
+                    exit 1
                     ;;
             esac
+            
             fi 
         else
-    
-        echo $1
+            echo $1
         fi
     fi
 }
+#====================================
+#====================================
 
 
-
-fi
-
-# 1 Idea Comprobar todo el array antes de calcular
-# 2 idea combrobar antes de cada calculo
--- ReadModule.sh
+++ ReadModule.sh
@@ -1,9 +1,89 @@
+#========= UPDATED 09/11/2018====
+#====================================
+#====================================
+#LECTIRA_IO + CASE ===========
+#====================================
+#====================================
+function lectura(){
 k=0
 while read
 do
         for i in `eval echo {1..$(echo $REPLY | wc -w|cut -c 8)}`;
         do
                 #m[$((k++))]="`echo $REPLY | cut -d ' ' -f $i`"
-                pushPila "`echo $REPLY | cut -d ' ' -f $i`"       
+        operando="`echo $REPLY | cut -d ' ' -f $i`"
+       
+
+        if ! [[ "$operando" =~ ^[0-9] ]];then
+            if [[ "$operando" = "*" ]];then
+                $operando = "POR"
+
+                #pullPila temp1
+                #pullPila temp2
+                #pushPila `multiplicar $temp1 $temp2`
+            elif [[ "$operando" = "!" ]];then
+                pullPila temp
+                pushPila `factorial $temp` 
+            else
+            case "$operando" in
+                "PRINT")
+                pullPila tPrint
+                    echo $tPrint #Experimento 2
+                 
+                    ;;
+                +)
+                        
+                    pullPila temp1
+                    pullPila temp2                
+                    pushPila  `suma $temp1 $temp2`
+                    
+                    ;;
+                -)  
+                    pullPila temp1 
+                    pullPila temp2               
+                    temp=$(resta $temp2 $temp1)
+                    pushPila $temp  
+                    ;;
+                /)
+                    pullPila temp1  
+                    pullPila temp2  
+                    temp=$(division $temp2 $temp1)                      
+                    pushPila $temp  
+                    ;;
+                "MOD")
+                    pullPila temp1  
+                    pullPila temp2  
+                    temp=$(mOdulo $temp2 $temp1)                      
+                    pushPila $temp  
+                    ;;
+                ^)
+                    #posible Eror
+                    pullPila temp1 # base 
+                    pullPila temp2 # exponente  
+                    temp=$(potencia $temp2 $temp1)
+                    pushPila $temp  
+                    ;;
+                "SQRT")
+                    pullPila temp1                      
+                    temp=$(raiz $temp1) 
+                    pushPila $temp  
+                    ;;
+                "POR")
+                pullPila temp1
+                pullPila temp2
+                pushPila `multiplicar $temp1 $temp2`
+                ;;
+            
+                *)
+                    echo "Syntax error CASE: $operando"
+                    exit 1
+                    ;;
+            esac
+            fi 
+        else
+           pushPila "$operando"       
+        fi
+                
         done
 done
+}
